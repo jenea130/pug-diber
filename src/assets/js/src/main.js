1405b8ffc93cffc9d230f7986dc1179ac307e1e4
jenea130 <jenea130@mail.ru>
2023-09-26 21:53:27 +0300

    feat: filter-drinks

--- src/assets/js/src/main.js
+++ src/assets/js/src/main.js
@@ -1,8 +1,10 @@
 import streetMap from "./modules/streetMap";
+import filterDrinks from "./modules/filterDrinks";
 import toggleMenu from "./modules/header/toggleMenu";
 
 window.addEventListener("DOMContentLoaded", function () {
     console.log("some thing for me");
     toggleMenu();
     streetMap();
+    filterDrinks();
 });
