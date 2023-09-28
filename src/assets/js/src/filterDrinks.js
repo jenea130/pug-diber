c93c1cedd15c8142566d28cd217c66314ce5b24c
jenea130 <jenea130@mail.ru>
2023-09-28 18:07:51 +0300

    fix: filterBeer

--- src/assets/js/src/filterDrinks.js
+++ src/assets/js/src/filterDrinks.js
@@ -1,21 +1,20 @@
 export default function filterDrinks() {
-  const filterItem = document.querySelectorAll(".filter-drinks__item");
-  const filterCategory = document.querySelectorAll(".filter-drinks__category");
+  const filterItemsAll = document.querySelectorAll(".filter-drinks__item");
+  const filterCategoriesAll = document.querySelectorAll("[data-target]");
 
-  filterCategory.forEach(item => {
+  filterCategoriesAll.forEach(item => {
     item.onclick = function() {
       
       // active 
-      filterCategory.forEach(item => {
+      filterCategoriesAll.forEach(item => {
         item.classList.remove("active");
       })
       item.classList.add("active");
 
       // filter
-      const text = item.textContent;
-      let value = text.split(',')[0];
+      const value = item.getAttribute('data-target');
     
-      filterItem.forEach(item => {
+      filterItemsAll.forEach(item => {
         item.style.display = "none";
         if (item.classList.contains(value.toLowerCase())) {
           item.style.display = "block";
